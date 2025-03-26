30a26e3a309d68281c61045b3c2ce68fe076cefe
Luis de Luna <ldg1008@alu.ubu.es>
2025-03-26 20:21:12 +0100

    #40 Opciones del workspace

--- project-app/Assets/Scripts/Options/WorkSpaceOptions.cs
+++ project-app/Assets/Scripts/Options/WorkSpaceOptions.cs
@@ -1,8 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Trabajo fin de grado 2024-2025 - VRLearnTOBOT
+ *
+ * Grado en Ingeniería Informática - Universidad de Burgos
+ *
+ * Autor: Luis Ignacio de Luna Gómez
+ * 
+ * email: ldg1008@alu.ubu.es
+ * 
+ * Fecha: 26/03/2025
+ * 
+ * Versión: 1.0.0
+ * 
+ * Descripción: Opciones para el espacio de trabajo
+ */
 
 public class WorkSpaceOptions
 {
