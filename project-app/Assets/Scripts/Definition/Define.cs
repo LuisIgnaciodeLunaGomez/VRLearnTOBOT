7be4067bdd984cbf77dc5c6e316d9887c2a256fa
Luis Ignacio de Luna Gómez <ldg1008@alu.ubu.es>
2025-04-06 22:03:52 +0200

    #46 Integración de la clase Define

---
+++ project-app/Assets/Scripts/Definition/Define.cs
@@ -0,0 +1,139 @@
+/*
+ * Trabajo fin de grado 2024-2025 - VRLearnTOBOT
+ *
+ * Grado en Ingeniería Informática - Universidad de Burgos
+ *
+ * Autor: Luis Ignacio de Luna Gómez
+ * 
+ * email: ldg1008@alu.ubu.es
+ * 
+ * Fecha:01/04/2025
+ * 
+ * Versión: 1.0.0
+ * 
+ * Descripción: Integración de la estructura de Ublockly dentro del proyecto por semejanza con ScratchBlocks. 
+ */
+
+using System;
+using System.Collections.Generic;
+
+
+public class Define
+{
+
+    /// <summary>
+    /// Lookup for the opposite type of a connection.
+    /// </summary>
+    public static EConnection OppositeConnection(EConnection connectionType)
+    {
+        switch (connectionType)
+        {
+            case EConnection.InputValue: return EConnection.OutputValue;
+            case EConnection.OutputValue: return EConnection.InputValue;
+            case EConnection.NextStatement: return EConnection.PrevStatement;
+            case EConnection.PrevStatement: return EConnection.NextStatement;
+        }
+        return EConnection.None;
+    }
+
+    /// <summary>
+    /// String for use in the 'custom' attribute of a category in toolbox.
+    /// This string indicates that the category shoud be dynamically populated with variable blocks
+    /// </summary>
+    public const string VARIABLE_CATEGORY_NAME = "VARIABLE";
+
+    /// <summary>
+    /// String for use in the 'custom' attribute of a category in toolbox.
+    /// This string indicates that the category shoud be dynamically populated with procedure blocks.
+    /// </summary>
+    public const string PROCEDURE_CATEGORY_NAME = "PROCEDURE";
+
+    public const string VARIABLE_GET_BLOCK_TYPE = "variables_get";
+    public const string VARIABLE_SET_BLOCK_TYPE = "variables_set";
+    public const string DEFINE_NO_RETURN_BLOCK_TYPE = "procedures_defnoreturn";
+    public const string DEFINE_WITH_RETURN_BLOCK_TYPE = "procedures_defreturn";
+    public const string CALL_NO_RETURN_BLOCK_TYPE = "procedures_callnoreturn";
+    public const string CALL_WITH_RETURN_BLOCK_TYPE = "procedures_callreturn";
+
+    /// <summary>
+    /// a list of field types defined in block
+    /// </summary>
+    public static string[] FIELD_TYPES = new string[]
+    {
+            "field_label", "field_input", "field_angle", "field_checkbox", "field_colour",
+            "field_variable", "field_dropdown", "field_image", "field_number", "field_date"
+    };
+
+    /// <summary>
+    /// a list of input types defined in block
+    /// </summary>
+    public static string[] INPUT_TYPES = new string[]
+    {
+            "input_value", "input_statement", "input_dummy"
+    };
+
+    /// <summary>
+    /// The custom defined datatype in blockly world
+    /// </summary>
+    public enum EDataType
+    {
+        Undefined = 0,
+        Boolean = 1,
+        Number = 2,        //int, float...
+        String = 3,
+        List = 4
+    }
+
+    public static Dictionary<EDataType, string[]> DataTypeDB = new Dictionary<EDataType, string[]>()
+        {
+            {EDataType.Boolean, new[] {"bool", "boolean"}},
+            {EDataType.Number, new[] {"float", "int", "double"}},
+            {EDataType.String, new[] {"string"}},
+            {EDataType.List, new[] {"ArrayList", "list"}}
+        };
+
+    /// <summary>
+    /// Configure if the field variable's dropdown options add manipulation options: rename, delete.
+    /// </summary>
+    public const bool FIELD_VARIABLE_ADD_MANIPULATION_OPTIONS = true;
+
+    /// <summary>
+    /// CLOCKWISE and OFFSET work together to set the behaviour of the angle picker.  
+    /// While many combinations are possible, two modes are typical: 
+    /// Math mode. 0 deg is right, 90 is up.  This is the style used by protractors.
+    ///    CLOCKWISE = false;
+    ///    OFFSET = 0; 
+    /// Compass mode. 0 deg is up, 90 is right.  This is the style used by maps.
+    ///    CLOCKWISE = true; 
+    ///    OFFSET = 90;
+    /// </summary>
+    public static bool FIELD_ANGLE_CLOCKWISE = true;
+    public static int FIELD_ANGLE_OFFSET = 90;
+
+    
+    /// <summary>
+    /// Maximum allowed angle before wrapping.
+    /// Usually either 360 (for 0 to 359.9) or 180 (for -179.9 to 180).
+    /// </summary>
+    public static int FIELD_ANGLE_WRAP = 360;
+
+    /// <summary>
+    /// Default width for field image
+    /// </summary>
+    public static int FIELD_IMAGE_WIDTH_DEFAULT = 30;
+    /// <summary>
+    /// Default height for field image
+    /// </summary>
+    public static int FIELD_IMAGE_HEIGHT_DEFAULT = 30;
+
+    
+    public const string CREATE_VARIABLE_TITLE = "MAKE_VARIABLE";
+    public const string CREATE_PROCEDURE_TITLE = "MAKE_PROCEDURE";
+
+    public const string RENAME_VARIABLE_OPTION_VALUE = "RENAME_VARIABLE_ID"; // Unique value for rename option
+    public const string DELETE_VARIABLE_OPTION_VALUE = "DELETE_VARIABLE_ID"; // Unique value for delete option
+    public const string NEW_VARIABLE_OPTION_VALUE = "NEW_VARIABLE_ID";
+
+    
+    public const string FIELD_IMAGE_SRC_DEFAULT = "Textures/Icons/fieldimage_default"; // Ajustar la carga de datos
+}
