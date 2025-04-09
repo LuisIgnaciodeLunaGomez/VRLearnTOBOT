c9bd2ad17094451859224073acf14bd899b70383
Luis Ignacio de Luna Gómez <ldg1008@alu.ubu.es>
2025-04-09 12:59:15 +0200

    #45

---
+++ project-app/Assets/Scripts/Types/FieldTypes.cs
@@ -0,0 +1,25 @@
+/*
+ * Trabajo fin de grado 2024-2025 - VRLearnTOBOT
+ *
+ * Grado en Ingeniería Informática - Universidad de Burgos
+ *
+ * Autor: Luis Ignacio de Luna Gómez
+ * 
+ * email: ldg1008@alu.ubu.es
+ * 
+ * Fecha: 01/04/2025
+ * 
+ * Versión: 1.0.0
+ * 
+ * Descripción:
+ */
+
+public static class FieldTypes
+{
+    public const string Label = "field_label"; 
+    public const string Input = "field_input";
+    public const string Dropdown = "field_dropdown";
+    // TODO otros tipos 
+    public const string Variable = "field_variable";
+
+}
